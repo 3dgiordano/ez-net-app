092a8915be93bbb3ced9061575d379e3f383e08f
David <3dgiordano@gmail.com>
2017-11-03 14:06:35 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var webdriverio = require('webdriverio');
 var options = {
+    logLevel: 'verbose',
     services: ['sauce'],
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
