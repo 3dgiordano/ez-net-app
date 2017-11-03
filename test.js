08e903a8bfb215491594d8f44e0344c85eebab91
David <3dgiordano@gmail.com>
2017-11-03 13:44:57 -0300

    Update test.js

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ var options = {
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
     desiredCapabilities: {
-        app:process.env.TRAVIS_BUILD_DIR + '/android/app/build/outputs/apk/app-debug.apk', 
-        platformName: ‘Android’,
+        app: process.env.TRAVIS_BUILD_DIR + '/android/app/build/outputs/apk/app-debug.apk', 
+        platformName: 'Android',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
     }
 };
