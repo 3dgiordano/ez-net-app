4a985c07419f9045eb7d840cd9379221af9d47d5
David <3dgiordano@gmail.com>
2017-11-03 16:56:20 -0300

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,5 +8,12 @@ exports.config = {
     services: ['sauce'],
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
-    sauceConnect: true
+    sauceConnect: true,
+    sauceConnectOpts: {
+        verbose: true,
+        verboseDebugging: true,
+        vv: true,
+        doctor: true,
+        tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+    }
 };
