b7e64e8fcdb8b152ef76047f9aa43996c4089912
David <3dgiordano@gmail.com>
2017-11-03 16:10:01 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var webdriverio = require('webdriverio');
 var options = {
     desiredCapabilities: {
+        logLevel: 'verbose',
         app: process.env.TRAVIS_BUILD_DIR + '/android/app/build/outputs/apk/app-debug.apk', 
         platformName: 'Android',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
