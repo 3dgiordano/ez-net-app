a55d6b8c04dc1b733c5bc99ea2f77bb1c427d98c
David <3dgiordano@gmail.com>
2017-11-03 17:24:40 -0300

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -4,7 +4,27 @@ exports.config = {
     specs: [
         path.join(__dirname, './test.js')
     ],
+    exclude: [
+    ],
+    maxInstances: 1,
+    sync: true,
+    coloredLogs: true,
     logLevel: 'verbose',
+    baseUrl: 'http://localhost:3000',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 300000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    reporters: [
+      'junit',
+      'spec'
+    ],
+    reporterOptions: {
+      outputDir: './artifacts/test'
+    },
+    mochaOpts: {
+        ui: 'bdd'
+    },
     services: ['sauce'],
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
@@ -16,8 +36,6 @@ exports.config = {
         doctor: true,
         tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
     },
-    capabilities: [{
-        
-    }],
-
+    capabilities: [
+    ]
 };
