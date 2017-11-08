a9ab7bd217127e0a89d89ea4b437c32201145c20
David <3dgiordano@gmail.com>
2017-11-08 10:39:49 -0300

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -34,7 +34,7 @@ exports.config = {
     services: ['sauce'],
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
-    sauceConnect: false,
+    sauceConnect: true,
     sauceConnectOpts: {
         /*verbose: true,
         verboseDebugging: true,
